dfa85dd795ddc21dd005166706f45405dc431232
Manishgupta3239 <guptamanish3239@gmail.com>
2025-04-10 20:30:32 +0530

    updated server js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
-import express, { json } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import ConnectionDb from "./mongodb/connection.js";
 import AuthRoutes from "./routes/authRoutes/AuthRoutes.js";
 import TaskRoutes from "./routes/taskRoutes/TaskRoutes.js";
-import bodyParser from "body-parser";
 import UserRoutes from "./routes/userRoutes/UserRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(cors({ origin: "https://management-system-2d8df.web.app", credentials: true }));
 app.use(express.json());
+app.use(cors({ origin: "https://management-system-2d8df.web.app", credentials: true }));
+
 app.use("/api/auth", AuthRoutes);
 app.use("/api/task", TaskRoutes);
 app.use("/api/admin", UserRoutes);
